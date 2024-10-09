837bdf1c4515a2cc86f05433dc5317f375d62cb2
JeckPGR <dzakyrazi@gmail.com>
2024-10-09 22:12:29 +0700

    buat login view

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -19,37 +19,40 @@
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
 </head>
 
-<body class="bg-gradient-primary">
+
+
+<body class="">
 
     <div class="container">
 
         <!-- Outer Row -->
-        <div class="row justify-content-center">
+        <div class="d-flex justify-content-center align-middle ">
 
-            <div class="col-xl-10 col-lg-12 col-md-9">
+            <div class="col-md-9 col-lg-6">
 
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
+                <div class="card o-hidden border-0 shadow shadow-blue my-5">
+
                         <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
-                                <div class="p-5">
+                        
+                            <div class="col-lg-12">
+                                <div class="py-5 px-4">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Hello There</h1>
                                     </div>
                                     <form class="user">
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
+                                            <input type="email" class="form-control "
                                                 id="exampleInputEmail" aria-describedby="emailHelp"
                                                 placeholder="Enter Email Address...">
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
+                                            <input type="password" class="form-control "
                                                 id="exampleInputPassword" placeholder="Password">
+                                        </div>
+                                        <div class="text-right">
+                                            <a class="small" href="forgot-password.html">Forgot Password?</a>
                                         </div>
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
@@ -58,28 +61,21 @@
                                                     Me</label>
                                             </div>
                                         </div>
-                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
+                                        <a href="index.html" class="btn btn-primary btn-block">
                                             Login
                                         </a>
                                         <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                        <a href="index.html" class="btn btn-google  btn-block">
+                                            <i class="fab fa-google fa-fw"></i> Login with SSO
                                         </a>
                                     </form>
                                     <hr>
-                                    <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
-                                    </div>
                                     <div class="text-center">
                                         <a class="small" href="register.html">Create an Account!</a>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
+                        
                 </div>
 
             </div>
