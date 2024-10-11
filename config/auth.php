72c71aa6d1bcfe9b651135277d5c06e94a05e75b
Walid Hanif Ataullah <walidhnf12@gmail.com>
2024-10-11 10:35:47 +0700

    pembuatan controller login, pembuatan tabler untuk memanggil header, sidebar, dan footer, serta pembuatan untuk ngepush script js.

--- config/auth.php
+++ config/auth.php
@@ -41,6 +41,11 @@ return [
             'provider' => 'users',
         ],
 
+        'user' => [
+            'driver' => 'session',
+            'provider' => 'users',
+        ],
+
         'api' => [
             'driver' => 'token',
             'provider' => 'users',
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\UsersController;
 use App\Models\Role;
 use App\Models\User;
 use App\Http\Controllers\TipePekerjaanController;
+use App\Http\Controllers\AuthController;
 
 
 /*
@@ -30,9 +31,7 @@ Route::get('/tipepekerjaan', function () {
     return view('tipepekerjaan');
 })->name('tipepekerjaan');
 
-Route::get('/index', function () {
-    return view('index');
-})->name('index');
+
 
 Route::get('/buttons', function () {
     return view('buttons');
@@ -46,18 +45,27 @@ Route::get('/blank', function () {
     return view('blank');
 })->name('blank');
 
+route::middleware(['guest:user'])->group(function () {
+    Route::get('/', function () {
+        return view('login');
+    })->name('login');
+     Route::post('/proseslogin', [AuthController::class, 'proseslogin'])->name('proseslogin');
+});
 
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
+route::middleware(['auth:user'])->group(function () {
+    Route::get('/index', function () {
+        return view('index');
+    })->name('index');
 
-Route::get('/addpegawai', [UsersController::class,'index'])->name('addpegawai');
+    Route::get('/addpegawai', [UsersController::class,'index'])->name('addpegawai');
+    Route::get('/tipepekerjaan', [TipePekerjaanController::class, 'index'])->name('tipepekerjaan');
+});
+
+Route::get('/proseslogout', [AuthController::class, 'proseslogout'])->name('proseslogout');
 Route::post('/addpegawai', [UsersController::class,'store'])->name('addpegawai.store');
 Route::delete('/addpegawai/{id}', [UsersController::class, 'destroy'])->name('addpegawai.destroy');
 Route::put('/addpegawai/{id}', [UsersController::class, 'update'])->name('addpegawai.update');
 
-
-Route::get('/tipepekerjaan', [TipePekerjaanController::class, 'index'])->name('tipepekerjaan');
 Route::post('/tipepekerjaan', [TipePekerjaanController::class, 'store'])->name('tipe_pekerjaan.store');
 Route::get('/editTipePekerjaan', [TipePekerjaanController::class, 'editTipePekerjaan']);
 Route::post('/tipepekerjaan/update/{id}', [TipePekerjaanController::class, 'update'])->name('tipepekerjaan.update');
